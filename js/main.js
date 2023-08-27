a603e91be684f6a13bc9d9b16fba3cef45a454b3
ibnuhibbann <ibnuhiban007@gmail.com>
2023-08-27 10:20:52 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+(function ($) {
+    "use strict";
+
+    // Initiate the wowjs
+    new WOW().init();
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $(".back-to-top").fadeIn("slow");
+        } else {
+            $(".back-to-top").fadeOut("slow");
+        }
+    });
+    $(".back-to-top").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+
+    // Product carousel
+    $(".product-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText: [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>',
+        ],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 3,
+            },
+        },
+    });
+})(jQuery);
