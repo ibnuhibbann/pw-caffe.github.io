5bfb7830acb8f86c527d03737f8c94d69ac50c16
ibnuhibbann <ibnuhiban007@gmail.com>
2023-08-28 13:08:58 +0800

    promo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
 (function ($) {
     "use strict";
 
+    // Promo carousel
+    $(".promo-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText: [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>',
+        ],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 3,
+            },
+        },
+    });
+
     // Initiate the wowjs
     new WOW().init();
 
